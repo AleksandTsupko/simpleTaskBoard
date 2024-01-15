87d3c9922ffae37f3ed0146c68a08fbf6f945940
Aleksandr <105147204+AleksandTsupko@users.noreply.github.com>
2024-01-15 21:03:55 +0400

    initial equipment

--- my_task_board/src/pages/HomePage/HomePage.tsx
+++ my_task_board/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,33 @@
+import { useRef, useState } from "react"
+import classes from './HomePage.module.scss'
+
 export function HomePage() {
-    return (   
-        <h3>Home</h3>
+    const [newBoardName, setNewBoardName] = useState("")
+    const boardNameInput = useRef<HTMLInputElement>(null)
+
+    const createHandler = (e:React.MouseEvent) => {
+        e.preventDefault()
+        if (newBoardName) {
+
+        } else if (boardNameInput.current) {
+            boardNameInput.current.classList.add(classes.wrong)
+            boardNameInput.current.focus()
+        }
+    }
+
+
+    return (
+        <>
+            <h3>Home</h3>
+            <form>
+                <input 
+                    ref={boardNameInput}
+                    type="text"
+                    value={newBoardName}
+                    onChange={e => setNewBoardName(e.target.value)}
+                />
+                <button onClick={(e) => createHandler(e)}>Create new board</button>
+            </form>
+        </>
     )
 }
